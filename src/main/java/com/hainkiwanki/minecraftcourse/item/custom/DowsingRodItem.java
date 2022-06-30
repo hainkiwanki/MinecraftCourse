71022307375cb7a58e6d29c9f95dc2d59a4d0933
hainkiwanki <kevinr.be@gmail.com>
2022-06-30 19:22:57 +0200

    Custom Tooltips

--- src/main/java/com/hainkiwanki/minecraftcourse/item/custom/DowsingRodItem.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/item/custom/DowsingRodItem.java
@@ -1,13 +1,22 @@
 package com.hainkiwanki.minecraftcourse.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DowsingRodItem extends Item {
 
@@ -41,6 +50,15 @@ public class DowsingRodItem extends Item {
                 (player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
 
         return super.useOn(pContext);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.minecraftcourse.dowsing_rod.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.minecraftcourse.dowsing_rod.tooltip"));
+        }
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block blockBelow) {
