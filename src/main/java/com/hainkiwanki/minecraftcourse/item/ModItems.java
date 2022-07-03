f3369eabe02984325fae46f2e0244e2a4d00e063
hainkiwanki <kevinr.be@gmail.com>
2022-07-03 12:02:04 +0200

    Creating tool set

--- src/main/java/com/hainkiwanki/minecraftcourse/item/ModItems.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/item/ModItems.java
@@ -3,8 +3,7 @@ package com.hainkiwanki.minecraftcourse.item;
 import com.hainkiwanki.minecraftcourse.MinecraftCourseMod;
 import com.hainkiwanki.minecraftcourse.item.custom.CoalSliverItem;
 import com.hainkiwanki.minecraftcourse.item.custom.DowsingRodItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +12,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MinecraftCourseMod.MOD_ID);
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
@@ -32,7 +34,18 @@ public class ModItems {
     public static final RegistryObject<Item> TURNIP = ITEMS.register("turnip",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB).food(ModFoods.TURNIP)));
 
-    public static void register(IEventBus eventBus){
-        ITEMS.register(eventBus);
-    }
+    public static final RegistryObject<Item> COBALT_SWORD = ITEMS.register("cobalt_sword",
+            () -> new SwordItem(ModTiers.COBALT, 2, 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_PICKAXE = ITEMS.register("cobalt_pickaxe",
+            () -> new PickaxeItem(ModTiers.COBALT, 2, 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_SHOVEL = ITEMS.register("cobalt_shovel",
+            () -> new ShovelItem(ModTiers.COBALT, 1, 3f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_AXE = ITEMS.register("cobalt_axe",
+            () -> new AxeItem(ModTiers.COBALT, 4, 0f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
+
+    public static final RegistryObject<Item> COBALT_HOE = ITEMS.register("cobalt_hoe",
+            () -> new HoeItem(ModTiers.COBALT, 0, 0f, new Item.Properties().tab(ModCreativeModeTab.COURSE_TAB)));
 }
