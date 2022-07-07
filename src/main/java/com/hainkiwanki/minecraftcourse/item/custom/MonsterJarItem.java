dd65b6a2f774c01ca8e301b740aef18691e1f15e
hainkiwanki <kevinr.be@gmail.com>
2022-07-07 20:49:40 +0200

    NBT Data exercise part 2: Finish

--- src/main/java/com/hainkiwanki/minecraftcourse/item/custom/MonsterJarItem.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/item/custom/MonsterJarItem.java
@@ -1,22 +1,17 @@
 package com.hainkiwanki.minecraftcourse.item.custom;
 
-import com.hainkiwanki.minecraftcourse.item.ModItems;
-import com.hainkiwanki.minecraftcourse.util.InventoryUtil;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -26,20 +21,13 @@ public class MonsterJarItem extends Item {
 
     @Override
     public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
-        //if (pInteractionTarget.level.isClientSide()) return InteractionResult.PASS;
         if (!pPlayer.level.isClientSide() && pUsedHand == InteractionHand.MAIN_HAND) {
-            if (pInteractionTarget instanceof Mob) {
-                Mob target = (Mob) pInteractionTarget;
+            if (pInteractionTarget instanceof LivingEntity) {
+                LivingEntity target = (LivingEntity) pInteractionTarget;
                 pPlayer.sendMessage(new TextComponent("item.minecraftcourse.monster_jar.clicked " + target.getClass().getName()), pPlayer.getUUID());
                 CompoundTag nbtData = new CompoundTag();
-                nbtData.putString("minecraftcourse.clicked_mob", "Mob clicked: " + target.getClass().getName());
-                ItemStack i = pPlayer.getInventory().getItem(InventoryUtil.getFirstInventoryIndex(pPlayer, ModItems.MONSTER_JAR.get()));
-                if(i == pStack) {
-                    pPlayer.sendMessage(new TextComponent("Same object"), pPlayer.getUUID());
-                } else {
-                    pPlayer.sendMessage(new TextComponent("Not same object"), pPlayer.getUUID());
-                }
-                pStack.setTag(nbtData);
+                nbtData.putString("minecraftcourse.clicked_mob", target.getClass().getSimpleName());
+                pPlayer.getItemInHand(InteractionHand.MAIN_HAND).setTag(nbtData);
                 return InteractionResult.SUCCESS;
             }
         }
