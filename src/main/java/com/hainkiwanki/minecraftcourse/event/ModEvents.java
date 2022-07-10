44ac21756703358b2a3c38f21f082a6bf1245af7
hainkiwanki <kevinr.be@gmail.com>
2022-07-10 18:10:44 +0200

    Custom Commands

---
+++ src/main/java/com/hainkiwanki/minecraftcourse/event/ModEvents.java
@@ -0,0 +1,29 @@
+package com.hainkiwanki.minecraftcourse.event;
+
+import com.hainkiwanki.minecraftcourse.MinecraftCourseMod;
+import com.hainkiwanki.minecraftcourse.command.ReturnHomeCommand;
+import com.hainkiwanki.minecraftcourse.command.SetHomeCommand;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+
+@Mod.EventBusSubscriber(modid = MinecraftCourseMod.MOD_ID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+        new SetHomeCommand(event.getDispatcher());
+        new ReturnHomeCommand(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerCloneEvent(PlayerEvent.Clone event) {
+        if(!event.getOriginal().level.isClientSide()) {
+            event.getPlayer().getPersistentData().putIntArray(MinecraftCourseMod.MOD_ID + "homepos",
+                    event.getOriginal().getPersistentData().getIntArray(MinecraftCourseMod.MOD_ID + "homepos"));
+        }
+    }
+}
