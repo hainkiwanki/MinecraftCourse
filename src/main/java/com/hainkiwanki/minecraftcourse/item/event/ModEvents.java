325c6fe96bd87868a7a7dcabc6c9e8e5bb5810f1
hainkiwanki <kevinr.be@gmail.com>
2022-07-11 19:41:57 +0200

    Events and Updating Forge and Parchment

--- src/main/java/com/hainkiwanki/minecraftcourse/item/event/ModEvents.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/item/event/ModEvents.java
@@ -3,7 +3,10 @@ package com.hainkiwanki.minecraftcourse.item.event;
 import com.hainkiwanki.minecraftcourse.MinecraftCourseMod;
 import com.hainkiwanki.minecraftcourse.command.ReturnHomeCommand;
 import com.hainkiwanki.minecraftcourse.command.SetHomeCommand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,6 +27,19 @@ public class ModEvents {
         if(!event.getOriginal().level.isClientSide()) {
             event.getPlayer().getPersistentData().putIntArray(MinecraftCourseMod.MOD_ID + "homepos",
                     event.getOriginal().getPersistentData().getIntArray(MinecraftCourseMod.MOD_ID + "homepos"));
+        }
+    }
+
+    @SubscribeEvent
+    public static void setEntityOnFireWhenHit(LivingDamageEvent event) {
+        if(!event.getEntity().level.isClientSide()) {
+            if(event.getSource().getDirectEntity() instanceof Player) {
+                Player player = (Player) event.getSource().getDirectEntity();
+                if(player.getMainHandItem().getItem() == Items.NETHER_BRICK) {
+                    player.getMainHandItem().shrink(1);
+                    event.getEntityLiving().setSecondsOnFire(2);
+                }
+            }
         }
     }
 }
