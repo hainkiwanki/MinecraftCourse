0fb39cadda806eb91def5e9cfdae18c7cec5cea8
hainkiwanki <kevinr.be@gmail.com>
2022-07-17 19:41:54 +0200

    Custom effect and potion

--- src/main/java/com/hainkiwanki/minecraftcourse/MinecraftCourseMod.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/MinecraftCourseMod.java
@@ -5,14 +5,17 @@ import com.hainkiwanki.minecraftcourse.block.ModWoodTypes;
 import com.hainkiwanki.minecraftcourse.block.entity.ModBlockEntities;
 import com.hainkiwanki.minecraftcourse.config.MinecraftCourseClientConfigs;
 import com.hainkiwanki.minecraftcourse.config.MinecraftCourseCommonConfigs;
+import com.hainkiwanki.minecraftcourse.effect.ModEffects;
 import com.hainkiwanki.minecraftcourse.enchantment.ModEnchantments;
 import com.hainkiwanki.minecraftcourse.fluid.ModFluids;
 import com.hainkiwanki.minecraftcourse.item.ModItems;
 import com.hainkiwanki.minecraftcourse.painting.ModPaintings;
+import com.hainkiwanki.minecraftcourse.potion.ModPotions;
 import com.hainkiwanki.minecraftcourse.recipe.ModRecipes;
 import com.hainkiwanki.minecraftcourse.screen.CobaltBlasterScreen;
 import com.hainkiwanki.minecraftcourse.screen.ModMenuTypes;
 import com.hainkiwanki.minecraftcourse.sound.ModSounds;
+import com.hainkiwanki.minecraftcourse.util.BetterBrewingRecipe;
 import com.hainkiwanki.minecraftcourse.util.ModItemProperties;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
@@ -20,11 +23,13 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.FlowerPotBlock;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -55,6 +60,8 @@ public class MinecraftCourseMod
         ModBlockEntities.register(eventBus);
         ModMenuTypes.register(eventBus);
         ModRecipes.register(eventBus);
+        ModEffects.register(eventBus);
+        ModPotions.register(eventBus);
 
         eventBus.addListener(this::setup);
         eventBus.addListener(this::clientSetup);
@@ -95,10 +102,12 @@ public class MinecraftCourseMod
             ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP_SEEDS.get(), 0.3f);
             ComposterBlock.COMPOSTABLES.put(ModItems.TURNIP.get(), 0.65f);
 
+
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.PINK_ROSE.getId(), ModBlocks.POTTED_PINK_ROSE);
 
             BlockEntityRenderers.register(ModBlockEntities.SIGN_BLOCK_ENTITIES.get(), SignRenderer::new);
             Sheets.addWoodType(ModWoodTypes.CHERRY_BLOSSOM);
+            BrewingRecipeRegistry.addRecipe(new BetterBrewingRecipe(Potions.AWKWARD, ModItems.COBALT_INGOT.get(), ModPotions.FREEZE_POTION.get()));
         });
     }
 }
