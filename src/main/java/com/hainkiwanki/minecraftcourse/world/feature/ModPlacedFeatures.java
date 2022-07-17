758236097b57de6585a7cf1dc44f9ff20989b812
hainkiwanki <kevinr.be@gmail.com>
2022-07-17 09:32:28 +0200

    Custom Tree Generation

--- src/main/java/com/hainkiwanki/minecraftcourse/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/world/feature/ModPlacedFeatures.java
@@ -1,16 +1,8 @@
 package com.hainkiwanki.minecraftcourse.world.feature;
 
-import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
-import net.minecraft.data.worldgen.placement.TreePlacements;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-
-import java.util.List;
 
 public class ModPlacedFeatures {
     public static final PlacedFeature CHERRY_BLOSSOM_PLACED = PlacementUtils.register("cherry_blossom_placed",
