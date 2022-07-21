c0b06677525283b18fbe916b66658753cbe46458
hainkiwanki <kevinr.be@gmail.com>
2022-07-21 09:53:43 +0200

    Raccoon animations

--- src/main/java/com/hainkiwanki/minecraftcourse/entity/custom/RaccoonEntity.java
+++ src/main/java/com/hainkiwanki/minecraftcourse/entity/custom/RaccoonEntity.java
@@ -16,6 +16,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
@@ -77,9 +81,20 @@ public class RaccoonEntity extends Animal implements IAnimatable {
 
     /* ANIMATIONS */
 
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        if (event.isMoving()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.raccoon.walk", true));
+            return PlayState.CONTINUE;
+        }
+
+        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.raccoon.idle", true));
+        return PlayState.CONTINUE;
+    }
+
     @Override
     public void registerControllers(AnimationData data) {
-
+        data.addAnimationController(new AnimationController(this, "controller",
+                0, this::predicate));
     }
 
     @Override
